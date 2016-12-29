8a0c4db3843a2d803876318d61de48da3bfff488
GCJ <kongsophanit221@gmail.com>
2016-12-29 11:44:02 -0800

    Commit and Push New Project

---
+++ MyApplication/app/src/main/java/com/sophanit/datasave/sqlitedatabasedemonstration/Communicator.java
@@ -0,0 +1,9 @@
+package com.sophanit.datasave.sqlitedatabasedemonstration;
+
+/**
+ * Created by GCJ on 0029-Dec-12-2016.
+ */
+
+public interface Communicator {
+    public void response(int position);
+}
